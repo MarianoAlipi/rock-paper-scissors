daa1e768cf6a9b8f1dd1660bd9b96335b8987823
Mariano Alipi <marianoalipi@gmail.com>
2020-11-06 11:53:55 -0600

    Access-Control-Allow testing.

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ app.use(express.json())
 app.use(express.urlencoded({
   extended: true
 }))
-app.use(cors())
+app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://marianoalipi.github.io https://www.github.com https://github.com");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // Connect to MongoDB.
 console.log("Connecting to MongoDB database...");
